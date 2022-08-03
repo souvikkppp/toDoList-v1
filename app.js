56d8c40efd290fd351f0723c188425fb2ea07aeb
soovik kundu <sovikkunducom19@gmail.com>
2022-08-03 19:09:50 +0530

    lec 268 complete

--- app.js
+++ app.js
@@ -4,55 +4,38 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-var today = new Date();//js method gives current date in a string ex: new Date(2022-08-01T15:15:48.034Z)
-var currentDay = today.getDay();//js method gives current day 0 to 6. 0 means sunday
-var day = "";
-// another way of doing this vol2
-// var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+app.use(bodyParser.urlencoded({
+  extended: true
+})); //grab info from the html using post method
+
 
 app.get("/", (req, res) => {
-  // another way of doing this vol2
+  var items = []; // this goes to our list.ejs using res.render and newTask ejs var
 
-  // if (currentDay === 6 || currentDay === 0) {
-  //   day = days[currentDay];
-  // } else {
-  //   day = days[currentDay];
-  // }
+  var today = new Date(); //js method gives current date in a string ex: new Date(2022-08-01T15:15:48.034Z)
+  var options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+  var day = today.toLocaleString("en-US", options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-      break;
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednesday"
-      break;
-    case 4:
-      day = "Thursday"
-      break;
-    case 5:
-      day = "Friday"
-      break;
-    case 6:
-      day = "Saturday"
-      break;
-    default:
 
-  }
   //this render ejs file which is named list.ejs
-  res.render("list", {
-    kindOfDay: day
-  });
-
+  res.render("list", { //we have to use all ejs variables in this place
+    kindOfDay: day,
+    newTasks: items //**we sending items arrey to our to do list in our list.ejs**//
+  }); //we sending that our user put on the form
 });
 
+app.post("/", (req, res) => {
+  var toDo = req.body.toDoTask;
+  items.push(toDo); //coz our res.render code is placed before our app.post
+  //we create this items var before and now pushing our
+  //post data (input by user) to items arrey
+  res.redirect("/");
+});
 
-console.log(today);
 
 
 
