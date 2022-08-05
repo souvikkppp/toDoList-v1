7dadd377aa9a578a5d69335bbf925323fd7ba188
soovik kundu <sovikkunducom19@gmail.com>
2022-08-05 08:57:24 +0530

    ejs lec 270 css style adding

--- app.js
+++ app.js
@@ -7,34 +7,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 })); //grab info from the html using post method
-
+app.use(express.static("public")); //to load static files like css and img
 
 app.get("/", (req, res) => {
-  var items = []; // this goes to our list.ejs using res.render and newTask ejs var
+  let items = ["Buy food","Cook food","Eat food"]; // this goes to our list.ejs using res.render and newTask ejs var
 
-  var today = new Date(); //js method gives current date in a string ex: new Date(2022-08-01T15:15:48.034Z)
-  var options = {
+  let today = new Date(); //js method gives current date in a string ex: new Date(2022-08-01T15:15:48.034Z)
+  let options = {
     weekday: "long",
     month: "long",
     day: "numeric"
   };
-  var day = today.toLocaleString("en-US", options);
+  let day = today.toLocaleString("en-US", options);
 
 
   //this render ejs file which is named list.ejs
   res.render("list", { //we have to use all ejs variables in this place
     kindOfDay: day,
-    newTasks: items //**we sending items arrey to our to do list in our list.ejs**//
-  }); //we sending that our user put on the form
+    newListItems: items
+  }); //**we sending items arrey to our to do list in our list.ejs**//
+  //we sending that our user put on the form
 });
 
 app.post("/", (req, res) => {
-  var toDo = req.body.toDoTask;
-  items.push(toDo); //coz our res.render code is placed before our app.post
-  //we create this items var before and now pushing our
-  //post data (input by user) to items arrey
+  let item = req.body.newItem;
+  items.push(item);
   res.redirect("/");
-});
+}); //coz our res.render code is placed before our app.post
+//we create this items var before and now pushing our
+//post data (input by user) to items arrey
 
 
 
